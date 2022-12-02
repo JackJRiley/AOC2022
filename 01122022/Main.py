59df8bf45ef61c68f203a2fc36ff4e2538e9279e
Jack Riley <jackjriley1@gmail.com>
2022-12-02 16:08:28 +0000

    Fixed indentation and comments for Day 1

--- 01122022/Main.py
+++ 01122022/Main.py
@@ -5,7 +5,7 @@ from obj.elf import elf
 with open("inputs/real_data.txt") as file:
     lines = file.read()
     groups = lines.split("\n\n")
-    #spawn elfs and give them their items
+    # spawn elfs and give them their items
     elf_house = []
     for items in groups:
         new_elf = elf()
@@ -13,23 +13,27 @@ with open("inputs/real_data.txt") as file:
             new_elf.take_to_inventory(item)
         elf_house.append(new_elf)
 
-    #ask each elf in the elf house to count their inventory
-    max = 0 
-    for e in elf_house:
-        answer = e.count_inventory()
-        if answer > max:
-            max = answer
+# ask each elf in the elf house to count their inventory
+max = 0 
+for e in elf_house:
+    answer = e.count_inventory()
+    if answer > max:
+        max = answer
         
-    #what is the max?
-    print(max)
+# what is the max?
+print(max)
 
 # Part 2 - how much is being carried by the top 3 elves 
 
-    # ask each elf how much they are carrying, but write it down
-    answers = []
-    for e in elf_house:
-        answer = e.count_inventory()
-        answers.append(answer)
-    sorted_answers = sorted(answers, reverse=True)
-    top3 = sum(sorted_answers[:3])
-    print(top3)
+# ask each elf how much they are carrying, but write it down
+answers = []
+for e in elf_house:
+    answer = e.count_inventory()
+    answers.append(answer)
+
+# sort them high to low
+sorted_answers = sorted(answers, reverse=True)
+
+# what is the sum of top 3
+top3 = sum(sorted_answers[:3])
+print(top3)
